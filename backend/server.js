0bd6cc73db3e92ab607299248e3a24d6c7d63dea
shubham-955 <shubhamgadge722@gmail.com>
2022-03-29 13:31:21 +0530

    post and get endpoints for specific user flight bookings added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const authRoute = require("./routes/auth")
+const bookedFlightRoute = require("./routes/BookedFlight")
 
 const connect = () => {
   return mongoose.connect(process.env.MONGOOSE_DB_URL);
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/auth", authRoute);
+app.use("/bookings", bookedFlightRoute);
 
 app.listen(1234, async (req, res) => {
   await connect();
