1fa25473288f82afbfe1ba34a2480888ae71bdb6
shubham-955 <shubhamgadge722@gmail.com>
2022-03-30 13:02:29 +0530

    added booked hotels schema and endpoints

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,18 @@ app.use(express.json());
 const hotelController = require("./controllers/hotels.controller.js")
 const authRoute = require("./routes/auth");
 const bookedFlightRoute = require("./routes/BookedFlight");
+const bookedHotelRoute = require("./routes/BookedHotel");
 const flightController = require("./controllers/flightController/flight.controller");
 
-
-app.use("/hotels", hotelController);
 app.use(cors());
 const connect = () => {
   return mongoose.connect(process.env.MONGOOSE_DB_URL);
 };
 
-
+app.use("/hotels", hotelController);
 app.use("/auth", authRoute);
 app.use("/bookings", bookedFlightRoute);
+app.use("/bookings", bookedHotelRoute);
 app.use("/flights", flightController);
 
 app.listen(1234, async (req, res) => {
--- frontend/src/features/Home/Home.jsx
+++ frontend/src/features/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SimpleSlider } from "../../Components/SimpleSlider";
 import { CarousalUI } from "../../Components/CarousalUI";
 import { FlightsCard } from "../../Components/FlightsCard";
-import  FlightHome  from "../../Components/FlightHome";
+import FlightHome from "../../Components/FlightHome";
 import Qna from "../../Hotels/Qna"
 import Details from "../../Hotels/Details"
 
@@ -11,46 +11,32 @@ import styles from "./home.module.css";
 export const Home = () => {
   return (
     <>
-    <FlightHome />
-  
-    <div className={styles.container}>
-      
-      <SimpleSlider />
-      <div style={{ marginTop: 50 }}>
-        <CarousalUI />
-      </div>
-      <div style={{ marginTop: 50, paddingLeft: 100, paddingRight: 100 }}>
-        <img
-          src="https://mmt.servedbyadbutler.com/getad.img/;libID=3405582"
-          style={{ width: "100%", height: "80%" }}
-        />
-      </div>
-      <div style={{ marginTop: 50 }}>
-        <FlightsCard />
-      </div>
-    </div>
-    <div className={styles.container}>
-      
-      <SimpleSlider />
-      <div style={{ marginTop: 50 }}>
-        <CarousalUI />
-      </div>
-      <div style={{ marginTop: 50, paddingLeft: 100, paddingRight: 100 }}>
-        <img
-          src="https://mmt.servedbyadbutler.com/getad.img/;libID=3405582"
-          style={{ width: "100%", height: "80%" }}
-        />
-      </div>
-      <div style={{ marginTop: 50 }}>
-        <FlightsCard />
-      </div>
-      
-      
-    </div>
-    <Details/>
-    <Qna/>
-    <Footer/>
+      <FlightHome />
 
-      </>
+      <div className={styles.container} style={{ boxSizing: 'border-box' }}>
+
+        <SimpleSlider />
+        <div style={{ marginTop: 50 }}>
+          <CarousalUI />
+        </div>
+        <div style={{ marginTop: 50, paddingLeft: 100, paddingRight: 100 }}>
+          <img
+            src="https://mmt.servedbyadbutler.com/getad.img/;libID=3405582"
+            style={{ width: "100%", height: "80%" }}
+          />
+        </div>
+        <div style={{ marginTop: 50 }}>
+          <FlightsCard />
+        </div>
+      </div>
+      <div className={styles.container}>
+
+
+        <Details />
+      </div>
+      <Qna />
+      <Footer />
+
+    </>
   );
 };
